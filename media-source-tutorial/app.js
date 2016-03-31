96c16f14e09d68ff69513cfbffa4a5b9a4d76cc7
lifangda02@gmail.com <lifangda02@gmail.com>
2016-03-31 08:15:05 -0300

    removed submodule

---
+++ media-source-tutorial/app.js
@@ -0,0 +1,37 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+    routes = require('./routes'),
+    http = require('http'),
+    path = require('path'),
+    request = require('request')
+
+// Start app
+var app = express();
+
+
+
+app.configure(function () {
+    app.set('views', __dirname + '/views');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.engine('html', require('ejs').renderFile);
+    app.use(express.errorHandler());
+});
+
+var server = app.listen(5000, function () {
+	console.log('Example app listening on port 5000!');
+});
+
+exports = module.exports = app;
+
+// Routes
+app.get('/basic', routes['basicPlayer']);
+app.get('/buffering', routes['bufferingPlayer']);
+app.get('/adaptive', routes['adaptiveStreamingPlayer']);
